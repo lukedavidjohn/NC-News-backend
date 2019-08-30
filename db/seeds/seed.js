a2530d6b74b6ca4467fa56d08ac8c320acb5f1d8
Luke Rushworth <ldjrushworth@icloud.com>
2019-08-30 17:13:48 +0100

    installs brcyptjs and adds encryption to password in seed file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,28 +1,40 @@
+const bcrypt = require("bcryptjs");
 const {
   topicData,
   articleData,
   commentData,
   userData
-} = require('../index.js');
+} = require("../index.js");
 
-const { formatDate, formatComments, makeRefObj } = require('../utils/utils');
+const { formatDate, formatComments, makeRefObj } = require("../utils/utils");
 
 exports.seed = function(knex, Promise) {
-  return knex.migrate.rollback().then(() => {
-    return knex.migrate.latest()
-  })
-  .then(()=> {
-    const topicsInsertions = knex('topics').insert(topicData);
-    const usersInsertions = knex('users').insert(userData);
-    return Promise.all([topicsInsertions, usersInsertions])
-  })
-  .then(() => {
-    const formattedArticleData = formatDate(articleData)
-    return knex('articles').insert(formattedArticleData).returning('*')
-  })
-  .then((articleInsertions) => {
-    const refObj = makeRefObj(articleInsertions);
-    const formattedComments = formatComments(commentData, refObj);
-    return knex('comments').insert(formattedComments).returning('*')
-  })
+  return knex.migrate
+    .rollback()
+    .then(() => {
+      return knex.migrate.latest();
+    })
+    .then(() => {
+      const topicsInsertions = knex("topics").insert(topicData);
+      let hashedUserData = [...userData];
+      let salt = bcrypt.genSaltSync(10);
+      hashedUserData.forEach((user, idx, arr) => {
+        arr[idx].password = bcrypt.hashSync(user.password, salt);
+      });
+      const usersInsertions = knex("users").insert(hashedUserData);
+      return Promise.all([topicsInsertions, usersInsertions]);
+    })
+    .then(() => {
+      const formattedArticleData = formatDate(articleData);
+      return knex("articles")
+        .insert(formattedArticleData)
+        .returning("*");
+    })
+    .then(articleInsertions => {
+      const refObj = makeRefObj(articleInsertions);
+      const formattedComments = formatComments(commentData, refObj);
+      return knex("comments")
+        .insert(formattedComments)
+        .returning("*");
+    });
 };
